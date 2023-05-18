a037f0b8519a3171818f68258e8210785e5130c1
Kanghyejin <rkdgpwlsz@naver.com>
2023-05-18 13:15:08 +0900

    CPU 인텐시브와 API 트러블 슈팅

--- class/02-cpu-intensive/src/app.controller.ts
+++ class/02-cpu-intensive/src/app.controller.ts
@@ -5,9 +5,17 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('/hello')
+  @Get('/qqq')
   getHello(): string {
-    const qqq = 3;
-    return this.appService.getHello();
+    let sum = 0;
+    for (let i = 0; i <= 9000000000000; i++) {
+      sum += i;
+    } // for문이 도는 동안 다른 함수를 실행하면 블락이 걸림 -> 멀티 스레드를 사용하여 블락을 막아야한다.
+    return '철수 성공';
+  }
+
+  @Get('/qqq2')
+  getHello2(): string {
+    return '영희 성공';
   }
 }
